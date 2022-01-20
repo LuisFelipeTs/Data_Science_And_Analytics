30839f06565901024a382d30dc11c81ddbc93de7
Luis Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-01-20 15:09:36 -0300

    Final Version
    Final version for the Basic Data Treatment with Python script (for N.A cases)

--- BasicDataTreatwPyforNAs.py
+++ BasicDataTreatwPyforNAs.py
@@ -12,15 +12,18 @@ from pandas.api.types import is_numeric_dtype
 file_l = " "
 n_data = pd.read_csv(file_l, sep = ";")
 
+#function for the specific pandas case (mode calculation) 
 def moda_calc(lista):
-    #function for the specific pandas case (mode calculation) 
     cases = {} 
+    v_list = []
     for i in lista:
         if (isinstance(i, float)): continue
-        elif i not in cases.keys(): cases[i] = 1
+        elif i not in cases.keys():
+            cases[i] = 1
+            v_list.append(i)
         else : cases[i] += 1
     chave_maj = max(cases, key=lambda key: cases[key])
-    print(chave_maj)
+    return(chave_maj, v_list)
 
 colnames = list(n_data.columns)
 for col in (colnames):
@@ -28,9 +31,8 @@ for col in (colnames):
         if is_numeric_dtype(n_data[col]): 
             n_data[col].fillna(stc.median(n_data[col]),inplace = True)
         elif is_string_dtype(n_data[col]):
-            mode = moda_calc(n_data[col])
-            #n_data[col].fillna(mode), inplace = True)
-            #print(n_data.loc[n_data[col] == float, col])
-            n_data.loc[n_data[col] == float, col] = mode
+            mode, v_list = moda_calc(n_data[col])
+            #n_data[col].fillna(mode), inplace = True
+            n_data.loc[~n_data[col].isin(v_list), col] = mode
     else: ("The column " + col +" is completed.")
 print(n_data)
