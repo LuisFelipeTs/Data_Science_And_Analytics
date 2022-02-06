4d44a676ba77fb7fb4166b6a739ae2026bc42443
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-02-06 14:13:12 -0300

    proportion hip_test commit

--- BasicDataTreat/hip_test_aux/hip_test_py_wProp.py
+++ BasicDataTreat/hip_test_aux/hip_test_py_wProp.py
@@ -1,1 +1,22 @@
 #hip_test_py_wProp
+
+import pandas as pd
+import statistics as st
+#import matplotlib.pyplot as mp
+import math as math
+
+data_base_old = pd.read_csv("", sep = ";") #old hip database
+alf = 0.05
+data_base_new = pd.read_csv("", sep = ";") #new hip database
+col_name = "" #Nome da coluna em ambas
+
+x = data_base_old[col_name]
+y = data_base_new[col_name]
+
+p0_med = st.mean(x)
+p_med = st.mean(y)
+y_dvp = st.stdev(y)
+n = len(y)
+
+result = (p_med - p0_med)/(math.sqrt(p0_med * (1 - p0_med)/ n ))
+print(result)
